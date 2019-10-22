f9d7c67dde129bdfc3814921cd1568924efc656b
deadman2000 <marchukov.ivan@gmail.com>
2019-10-22 21:35:32 +0300

    Notabenoid Patch Win32

--- Notabenoid_Patch/publish.cmd
+++ Notabenoid_Patch/publish.cmd
@@ -1,8 +1,6 @@
-RD /S /Q publish
+dotnet publish -r %1 -c Release -o publish/%1/
 
-dotnet publish -c Release -o publish/
+XCOPY patch.bat publish\%1\
+XCOPY README.TXT publish\%1\
 
-XCOPY patch.bat publish\
-XCOPY README.TXT publish\
-
-XCOPY /E ..\Conquest publish\Conquest\
+XCOPY /E ..\Conquest publish\%1\Conquest\
---
+++ Notabenoid_Patch/publish_all.cmd
@@ -0,0 +1,4 @@
+RD /S /Q publish
+
+call publish.cmd win-x64
+call publish.cmd win-x86
